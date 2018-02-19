57bb57a9c8983534721a634d3c02b19a0402b4de
Imelda Tirta Lie <danilihardja@danis-macbook-air.local>
2018-02-20 00:04:58 +0700

    Initial Commit

---
+++ BeerSalery/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BeerSalery
+//
+//  Created by Dani Lihardja on 2/20/18.
+//  Copyright © 2018 Prince Hendrie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
